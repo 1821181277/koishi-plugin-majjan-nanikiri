020a91a1211ccfe44336e61ce0656acb7993a700
Kumin <43703690+1821181277@users.noreply.github.com>
2025-01-06 14:38:20 +0800

    fix: 对多个会话同时使用何切指令的支持

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,7 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({});
 
-let currentNo = 0;
-
+let idQNoMap = new Map<string, number>();
 export function apply(ctx: Context) {
   ctx.command("何切").action(async ({ session }) => {
     if (!session) return "无法获取会话信息";
@@ -15,19 +14,21 @@ export function apply(ctx: Context) {
     const aDataMap = await getNanikiriAData();
     let totalCnt = qDataMap.size;
     let data;
-    if (currentNo == 0) {
+    let currentNo = idQNoMap.get(session.channelId);
+    if (currentNo == 0 || currentNo == null) {
       //随机抽取一道题目
       let nanikiriNo = getRandomInt(totalCnt);
       currentNo = nanikiriNo;
+      idQNoMap.set(session.channelId, currentNo);
       data = qDataMap.get(nanikiriNo);
     } else {
       //发布答案
       data = aDataMap.get(currentNo);
       currentNo = 0;
+      idQNoMap.set(session.channelId, currentNo);
     }
 
     // 从洗好的牌组中抽取一张牌
-
     await session.send(`
       ${h("at", { id: session.userId })}
       ${h.image(data.picBuffer, "image/webp")}
